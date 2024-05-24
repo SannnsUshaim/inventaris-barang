17d5893b2d857f396bcc0b69b8fdd6ac2b59fed9
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-24 19:38:51 +0700

    add: dark mode view

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white w-full px-4 py-2 flex justify-between items-center rounded-md font-medium text-sm">
+<footer class="bg-white dark:bg-gray-800 dark:text-white w-full px-4 py-2 flex justify-between items-center rounded-md font-medium text-sm">
     <p>&copy; Muhammad Ihsan Ushaim | XI RPL 1</p>
     <p id="date-time"></p>
 </footer>
