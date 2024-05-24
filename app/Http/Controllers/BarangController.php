ee6308e665a90c29891b4c69e54c04c5ed5f1041
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-24 19:34:36 +0700

    modify: updata function

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -8,15 +8,29 @@ use Illuminate\View\View;
 
 class BarangController extends Controller
 {
+    /**
+     * index
+     * @return View
+     */
     public function index():View {
         $barang = Barang::latest()->paginate(10);
         return view("barang.index", compact("barang"));
     }
 
+    /**
+     * create
+     * @return View
+    */ 
     public function create() {
         return view("barang.create");
     }
 
+    /**
+     * stor
+     * @param mixed $request
+     * @param mixed $id
+     * @return RedirectResponse
+     */
     public function store(Request $request):RedirectResponse {
         $this->validate($request, [
             'id_barang' => 'required | min:2',
@@ -33,24 +47,39 @@ class BarangController extends Controller
     }
 
     public function edit(string $id):View {
-        $data = Barang::find($id);
+        $data = Barang::findOrFail($id);
         return view('barang.edit', compact('data'));
     }
 
-    public function update(Request $request, string $id): RedirectResponse {
+    /**
+    * update
+    *
+    * @param mixed $request
+    * @param mixed $id
+    * @return RedirectResponse
+    */
+    public function update(Request $request, $id): RedirectResponse {
         $this->validate($request, [
-            'nama_barang' => 'required | min:2',
-            'jenis_barang' => 'required | min:2',
-            'merek'=> 'required | min:2',
-            'jumlah'=> 'required',
-            'harga'=> 'required',
+            'nama_barang' => 'required|min:2',
+            'jenis_barang' => 'required|min:2',
+            'merek' => 'required|min:2',
+            'jumlah' => 'required|numeric',
+            'harga' => 'required|numeric',
         ]);
 
-        return redirect()->route('barang.index')->with('success','Data Behasil Diubah!');
+        $data = Barang::findOrFail($id);
+
+        $data->update($request->only(['nama_barang', 'jenis_barang', 'merek', 'jumlah', 'harga']));
+
+        return redirect()->route('barang.index')->with('success', 'Data berhasil diubah!');
     }
 
-    public function destroy($id_barang): RedirectResponse {
-        $data = Barang::findOrFail($id_barang);
+    /**
+     * @param mixed $id
+     * @return RedirectResponse
+     */
+    public function destroy($id): RedirectResponse {
+        $data = Barang::findOrFail($id);
 
         $data->delete();
 
