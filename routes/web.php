9b3e9697e77478c9f70ff4a3a86fabf494c354ef
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-24 19:39:20 +0700

    modify: route::patch to route::put

--- routes/web.php
+++ routes/web.php
@@ -35,45 +35,27 @@ Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
     Route::get('barang', [BarangController::class, 'index'])->name('barang.index');
     Route::get('barang/create', [BarangController::class, 'create'])->name('barang.create');
     Route::post('barang/store', [BarangController::class, 'store'])->name('barang.store');
-    Route::get('barang/{id_barang}/edit', [BarangController::class, 'edit'])->name('barang.edit');
-    Route::patch('barang/{id_barang}/update', [BarangController::class, 'update'])->name('barang.update');
-    Route::delete('barang/{id}/delete', [BarangController::class, 'destroy'])->name('barang.destroy');
+    Route::get('barang/{id}/edit', [BarangController::class, 'edit'])->name('barang.edit');
+    Route::put('barang/{id}/update', [BarangController::class, 'update'])->name('barang.update');
+    Route::delete('barang/delete', [BarangController::class, 'destroy'])->name('barang.destroy');
 });
 
 Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
     Route::get('pembelian', [PembelianController::class, 'index'])->name('pembelian.index');
     Route::get('pembelian/create', [PembelianController::class, 'create'])->name('pembelian.create');
-    Route::post('barang/store', [PembelianController::class, 'store'])->name('pembelian.store');
-    Route::get('pembelian/{id_pembelian}/edit', [PembelianController::class, 'edit'])->name('pembelian.edit');
-    Route::patch('pembelian/{id_pembelian}/update', [PembelianController::class, 'update'])->name('pembelian.update');
-    Route::delete('pembelian/{id}/delete', [PembelianController::class, 'destroy'])->name('pembelian.destroy');
+    Route::post('pembelian/store', [PembelianController::class, 'store'])->name('pembelian.store');
+    Route::get('pembelian/{id}/edit', [PembelianController::class, 'edit'])->name('pembelian.edit');
+    Route::put('pembelian/{id}/update', [PembelianController::class, 'update'])->name('pembelian.update');
+    Route::delete('pembelian/delete', [PembelianController::class, 'destroy'])->name('pembelian.destroy');
 });
 
 Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
     Route::get('pemakaian', [PemakaianController::class, 'index'])->name('pemakaian.index');
     Route::get('pemakaian/create', [PemakaianController::class, 'create'])->name('pemakaian.create');
     Route::post('pemakaian/store', [PemakaianController::class, 'store'])->name('pemakaian.store');
-    Route::get('pemakaian/{id_pemakaian}/edit', [PemakaianController::class, 'edit'])->name('pemakaian.edit');
-    Route::patch('pemakaian/{id_pemakaian}/update', [PemakaianController::class, 'update'])->name('pemakaian.update');
-    Route::delete('pemakaian/{id}/delete', [PemakaianController::class, 'destroy'])->name('pemakaian.destroy');
-});
-
-// Grouping routes with common middleware 'auth' and 'verified'
-Route::middleware(['auth', 'verified'])->group(function () {
-    // Admin route
-    Route::get('admin', function() {
-        return view('layouts.admin.index');
-    })->middleware('role:admin')->name('admin.index');
-
-    // Operator route
-    Route::get('operator', function() {
-        return view('layouts.user.index');
-    })->middleware('role:operator|admin')->name('operator.index');
-
-    // Staff route
-    Route::get('staff', function() {
-        return view('layouts.user.index');
-    })->middleware('role:staff|admin')->name('staff.index');
+    Route::get('pemakaian/{id}/edit', [PemakaianController::class, 'edit'])->name('pemakaian.edit');
+    Route::put('pemakaian/{id}/update', [PemakaianController::class, 'update'])->name('pemakaian.update');
+    Route::delete('pemakaian/delete', [PemakaianController::class, 'destroy'])->name('pemakaian.destroy');
 });
 
 require __DIR__.'/auth.php';
