1063ab30d465ae5865a37bd9b713651c261146fb
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-24 19:38:10 +0700

    modify: dashboard admin view

--- resources/views/layouts/admin/index.blade.php
+++ resources/views/layouts/admin/index.blade.php
@@ -10,7 +10,7 @@
         <div class="grid grid-cols-12 gap-4">
             <div class="col-span-8 row-span-1">
                 <div class="bg-slate-800 flex gap-3 text-white p-6 rounded-md justify-between">
-                    <p class="text-xl font-semibold">Wellcome back {{ Auth::user()->name }}!</p>
+                    <p class="text-xl font-semibold">Welcome back {{ Auth::user()->name }}!</p>
                     <img src="{{ asset('assets/img/Pie-chart.gif') }}" alt="chart" class="w-[100px]">
                 </div>
             </div>
@@ -30,16 +30,16 @@
                 <a href="{{ route('pembelian.index') }}" class="bg-white dark:bg-gray-700 dark:text-white text-black pl-8 pr-4 py-6 flex items-center transition hover:scale-105 rounded-md gap-3 hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:bg-gradient-to-tr hover:from-teal-500 hover:to-teal-200 hover:!text-white col-span-3">
                     <div class="flex flex-col text-right font-semibold">
                         <p class="text-lg">Data Pembelian</p>
-                        <p class="font-bold text-2xl">9999</p>
+                        <p class="font-bold text-2xl">{{ $pembelian->count() }}</p>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 opacity-70">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                     </svg>
                 </a>
-                <a href="" class="bg-white dark:bg-gray-700 dark:text-white text-black pl-8 pr-4 py-6 flex items-center transition hover:scale-105 rounded-md gap-3 hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:bg-gradient-to-tr hover:from-teal-500 hover:to-teal-200 hover:!text-white col-span-3">
+                <a href="{{ route('pemakaian.index') }}" class="bg-white dark:bg-gray-700 dark:text-white text-black pl-8 pr-4 py-6 flex items-center transition hover:scale-105 rounded-md gap-3 hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:bg-gradient-to-tr hover:from-teal-500 hover:to-teal-200 hover:!text-white col-span-3">
                     <div class="flex flex-col text-right font-semibold">
                         <p class="text-lg">Data Pemakaian</p>
-                        <p class="font-bold text-2xl">9999</p>
+                        <p class="font-bold text-2xl">{{ $pemakaian->count() }}</p>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 opacity-70">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
@@ -48,7 +48,7 @@
                 <a href="" class="bg-white dark:bg-gray-700 dark:text-white text-black pl-8 pr-4 py-6 flex items-center transition hover:scale-105 rounded-md gap-3 hover:translate-x-1 hover:translate-y-1 hover:shadow-md hover:bg-gradient-to-tr hover:from-teal-500 hover:to-teal-200 hover:!text-white col-span-3">
                     <div class="flex flex-col text-right font-semibold">
                         <p class="text-lg">Data Pembelian</p>
-                        <p class="font-bold text-2xl">9999</p>
+                        <p class="font-bold text-2xl">99999</p>
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 opacity-70">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
