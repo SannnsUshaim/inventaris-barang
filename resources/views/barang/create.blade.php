2a8e68bfc14b41b740fe8521e05847eb1b34f25d
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-24 19:36:15 +0700

    add: view dark mode

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -13,36 +13,36 @@
     </x-slot>
 
 
-    <div class="bg-white flex flex-col w-full h-full rounded-md shadow-md p-6 gap-3">
+    <div class="bg-white dark:bg-gray-800 flex flex-col w-full h-full rounded-md shadow-md p-6 gap-3">
         <form action="{{ route('barang.store') }}" enctype="multipart/form-data" class="max-w-md flex flex-col justify-between h-full" method="POST">
             @csrf
             <div class="flex flex-col gap-3">
-                <div class="flex flex-col gap-2">
+                <div class="flex flex-col gap-2 dark:text-white">
                     <label class="text-sm" for="id_barang">Kode Barang</label>
-                    <input type="text" class="px-2 py-1 text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-md focus:outline-none" name="id_barang" required>
+                    <input type="text" class="px-2 py-1 !text-black text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-sm focus:outline-none" name="id_barang" required>
                 </div>
-                <div class="flex flex-col gap-2">
+                <div class="flex flex-col gap-2 dark:text-white">
                     <label class="text-sm" for="id_barang">Nama Barang</label>
-                    <input type="text" class="px-2 py-1 text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-md focus:outline-none" name="nama_barang" required>
+                    <input type="text" class="px-2 py-1 !text-black text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-sm focus:outline-none" name="nama_barang" required>
                 </div>
-                <div class="flex flex-col gap-2">
+                <div class="flex flex-col gap-2 dark:text-white">
                     <label class="text-sm" for="id_barang">Jenis Barang</label>
-                    <input type="text" class="px-2 py-1 text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-md focus:outline-none" name="jenis_barang" required>
+                    <input type="text" class="px-2 py-1 !text-black text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-sm focus:outline-none" name="jenis_barang" required>
                 </div>
-                <div class="flex flex-col gap-2">
+                <div class="flex flex-col gap-2 dark:text-white">
                     <label class="text-sm" for="id_barang">Merek</label>
-                    <input type="text" class="px-2 py-1 text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-md focus:outline-none" name="merek" required>
+                    <input type="text" class="px-2 py-1 !text-black text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-sm focus:outline-none" name="merek" required>
                 </div>
-                <div class="flex flex-col gap-2">
+                <div class="flex flex-col gap-2 dark:text-white">
                     <label class="text-sm" for="id_barang">Jumlah</label>
-                    <input type="number" class="px-2 py-1 text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-md focus:outline-none" name="jumlah" required>
+                    <input type="number" class="px-2 py-1 !text-black text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-sm focus:outline-none" name="jumlah" required>
                 </div>
-                <div class="flex flex-col gap-2">
+                <div class="flex flex-col gap-2 dark:text-white">
                     <label class="text-sm" for="id_barang">Harga</label>
-                    <input type="number" class="px-2 py-1 text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-md focus:outline-none" name="harga" required>
+                    <input type="number" class="px-2 py-1 !text-black text-base font-medium rounded-lg border-[1.7px] border-gray-300 shadow-sm focus:outline-none" name="harga" required>
                 </div>
             </div>
-            <div class="flex gap-3 items-center">
+            <div class="flex gap-3 items-center dark:text-white">
                 <button type="submit" class="bg-blue-500 px-4 py-1 rounded-md text-white transition hover:opacity-80">Submit</button>
                 <a href="{{ route('barang.index') }}">Cancel</a>
             </div>
