1fdf343f1429a51012389cd7a6d2c2b81b222ba7
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-28 23:14:24 +0700

    add: pemakaian create

--- app/Http/Controllers/PemakaianController.php
+++ app/Http/Controllers/PemakaianController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Pemakaian;
+use App\Models\Barang;
 use Illuminate\Http\Request;
 
 class PemakaianController extends Controller
@@ -9,5 +10,10 @@ class PemakaianController extends Controller
     public function index() {
         $pemakaian = Pemakaian::latest()->paginate(10);
         return view("pemakaian.index", compact("pemakaian"));
+    }
+
+    public function create() {
+        $barang = Barang::all();
+        return view("pemakaian.create", compact("barang"));
     }
 }
