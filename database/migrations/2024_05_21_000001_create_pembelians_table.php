be95d1865e4dbd4a63367e622066a340083f1d66
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-28 23:15:53 +0700

    add: schema pembelian

---
+++ database/migrations/2024_05_21_000001_create_pembelians_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembelians', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_barang');
+            $table->foreign('id_barang')->references('id_barang')->on('barangs')->onDelete('cascade');
+            $table->integer('jumlah');
+            $table->integer('harga');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembelians');
+    }
+};
