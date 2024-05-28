33e34619c26b5e57321289b1266129d4cabc41b4
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-28 23:09:35 +0700

    modify: don't know

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white dark:bg-gray-800 dark:text-white w-full px-4 py-2 flex justify-between items-center rounded-md font-medium text-sm">
+<footer class="bg-white dark:bg-gray-800 dark:text-white w-full px-4 py-2 flex justify-between items-center rounded-md font-medium text-sm bottom-0 relative">
     <p>&copy; Muhammad Ihsan Ushaim | XI RPL 1</p>
     <p id="date-time"></p>
 </footer>
