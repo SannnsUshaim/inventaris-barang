2522b5e6a48407f43dc66de33822debbaf1f1a92
SannnsUshaim <ushaimihsan@gmail.com>
2024-05-28 23:13:46 +0700

    add: latest data and user

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Barang;
 use App\Models\Pembelian;
 use App\Models\Pemakaian;
+use App\Models\User;
 
 
 use Illuminate\Routing\Controller as BaseController;
@@ -19,10 +20,15 @@ class Controller extends BaseController
     public function dashboard() {
         if (auth()->user()->hasRole("admin")) {
             $username = auth()->user()->name;
+            $barang_latest = Barang::latest()->paginate(1);
+            $pembelian_latest = Pembelian::join("barangs","pembelians.id_barang","=","barangs.id_barang")->select("barangs.nama_barang","pembelians.jumlah","pembelians.created_at")->latest()->paginate(1);
+            $pemakaian_latest = Pemakaian::latest()->paginate(1);
             $barang = Barang::all();
             $pembelian = Pembelian::all();
             $pemakaian = Pemakaian::all();
-            return view('layouts.admin.index', compact('username', 'barang', 'pembelian', 'pemakaian'));
+            $user = User::all();
+            $user_latest = User::latest()->paginate(1);
+            return view('layouts.admin.index', compact('username', 'barang', 'pembelian', 'pemakaian', 'barang_latest', 'pembelian_latest', 'pemakaian_latest', 'user', 'user_latest'));
         }
         else {
             return view('layouts.user.index');
